39518ff17c824f9740797d13ece0777d9dea8ff6
jay23cee <jayceexxiii@gmail.com>
2021-03-25 23:38:54 -0700

    simplify the actions fucntions

--- my-app/src/store/books/bookAction.ts
+++ my-app/src/store/books/bookAction.ts
@@ -9,44 +9,34 @@ import {
   import {AppAction} from "../books/actionType";
   import {Books} from "../books/books";
   import AppState from "../store"
+import { bookReducer } from "./bookReducer";
 
-  export const FetchBook = (books: Books[]):AppAction=>({
-      type: FETCH_BOOK,
-      books,
-  })
+ 
 
-  export const EditBook = (books: Books):AppAction=>({
-    type: EDIT_BOOK,
-    books,
-})
-
-export const DeleteBook = (books: Books):AppAction=>({
-    type: DELETE_BOOK,
-    books,
-})
-
-
-export const NewBook = (books: Books):AppAction=>({
-    type: NEW_BOOK,
-    books,
-})
 export const startEditBook= (book:Books) => {
     return ( dispatch: Dispatch<AppAction>, getState:() => typeof AppState) => {
-        dispatch(EditBook(book));
+        dispatch({
+            type: EDIT_BOOK,
+            books:book});
     }
 }
 
 export const startNewBook =(book:Books) =>{
-    return ( dispatch: Dispatch<AppAction>, getState:() => typeof AppState) =>{
-        dispatch(NewBook(book));
+    return ( dispatch: Dispatch<AppAction>, getState:() => typeof AppState) => {
+        dispatch({
+            type: NEW_BOOK,
+            books:book});
     }
 }
 
-export const startFetchBook=(book:Books) =>{
-    return( dispatch: Dispatch<AppAction>, getState:()=> typeof AppState) => {
-        dispatch(NewBook(book))
-    }
-}
+// export const startNewBook=(book:Books) =>{
+//     return ( dispatch: Dispatch<AppAction>, getState:() => typeof AppState) => {
+//         dispatch({
+//             type: NEW_BOOK,
+//             books:book});
+//     }
+    
+// }
 
 
 
