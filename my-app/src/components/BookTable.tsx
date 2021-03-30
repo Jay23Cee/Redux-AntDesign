445eb8ee2d45df135356823e35df8a41381a4619
jay23cee <jayceexxiii@gmail.com>
2021-03-29 22:22:22 -0700

    update booktable

--- my-app/src/components/BookTable.tsx
+++ my-app/src/components/BookTable.tsx
@@ -1,111 +1,261 @@
-import React, { useState } from 'react';
+import React, { useState,  Component } from 'react';
 import { Table, Input, InputNumber, Popconfirm, Form, Typography } from 'antd';
-import {Books} from '../store/books/books'
-import {AppState} from '../store/store';
-import {connect} from 'react-redux';
-import {AppAction} from '../store/books/actionType';
-import {ThunkDispatch} from "redux-thunk";
-import * as action from "../store/books/bookAction";
-import {bindActionCreators} from 'redux';
-import { ProgressPlugin } from 'webpack';
+import { timeStamp } from 'node:console';
+import { kMaxLength } from 'node:buffer';
+
+import {Book} from '../store/book/Book';
+
+import {AppState} from "../store/store";
+import { connect} from 'react-redux';
+import {AppAction } from "../store/book/actionType";
+import { ThunkDispatch  } from "redux-thunk";
+import * as action from "../store/book/bookAction";
+import { bindActionCreators } from 'redux';
 
 
-interface BookTableProps{
-  title?: string;
-  author?: string;
-  date?:string;
+
+
+export interface BookTableProps{
+  title: string;
+  author: string;
+  date: string;
+  key: string;
 }
 
 interface BookTableState {}
 
 type Props = BookTableProps & LinkStateProps & LinkDispatchProps;
 
+export class BookTable extends React.Component<Props, BookTableState> {
 
-export class BookTable extends React.Component<Props, BookTableState>{
-  onEdit = (book: Books) => {
-    this.props.startEditBook(book);
-  };
+  // onRemove = (id: string) => {
+  //   this.props.startRemoveExpense(id);
+  // };
+  render() {
 
+    const { originData} = this.props;
+    
+    const EditableTable = () => {
+      const [form] = Form.useForm();
+      const [data, setData] = useState(originData);
+      const [editingKey, setEditingKey] = useState('');
+    
+      const isEditing = (record: Book) => record.key === editingKey;
+      const isDeleting  = (record: Book) => record.key === editingKey;
+    
+  
+      const onEdit = (record: Partial<Book> & { key: React.Key }) => {
+        form.setFieldsValue({ title: '', author: '', date: '', address: '', ...record });
+        setEditingKey(record.key);
+       
+      };
+    
+    
+    
+      const onDelete = (record: Partial<Book> & { key: React.Key }) => {
+        setEditingKey(record.key);
+        this.props.startDeleteBook(record.key)
+     
+      };
+    
+     
+    
+      const cancel = () => {
+        setEditingKey('');
+      };
+    
+    
+    
+      const save = async (key: React.Key) => {
+        try {
+          const row = (await form.validateFields()) as Book;
+    
+          const newData = [...data];
+          const index = newData.findIndex(item => key === item.key);
+          if (index > -1) {
+            const item = newData[index];
+            newData.splice(index, 1, {
+              ...item,
+              ...row,
+            });
+            setData(newData);
+            console.log(newData[index]);
+          this.props.startEditBook(newData[index]);
+            setEditingKey('');
+          
+          } else {
+            newData.push(row);
+            setData(newData);
+            setEditingKey('');
+          }
+        } catch (errInfo) {
+          console.log('Validate Failed:', errInfo);
+        }
+      };
+    
+      /**************************
+       ******* Columns **********
+       ******** of the *********
+       ********* Table *********/
+      const columns = [
+        {
+          title: 'title',
+          dataIndex: 'title',
+          width: '45%',
+          editable: true,
+        },
+        {
+          title: 'author',
+          dataIndex: 'author',
+          width: '25%',
+          editable: true,
+        },
+        {
+          title: 'date',
+          dataIndex: 'date',
+          width: '15%',
+          editable: false,
+        },
+        {
+          title: 'action',
+          dataIndex: 'action',
+          render: (_: any, record: Book) => {
+            const editable = isEditing(record) || isDeleting(record);
+            return editable ? (
+              <span>
+                <a href="javascript:;" onClick={() => save(record.key)} style={{ marginRight: 8 }}>
+                  Save
+                </a>
+                <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+                  <a>Cancel</a>
+                </Popconfirm>
+              </span>
+              
+            ) : (
+              <Typography.Link >
+                <Typography.Link disabled={editingKey !== ''} onClick={() =>onEdit(record)}>
+                 Edit
+                </Typography.Link>
+                <br></br>
+                <Typography.Link disabled={editingKey !== ''} onClick={() => onDelete(record)}>
+                 Delete
+              </Typography.Link>
+    
+              
+    
+              </Typography.Link>
+    
+              
+            );
+          },
+        },
+    
+      ];
+    
+      const mergedColumns = columns.map(col => {
+        if (!col.editable) {
+          return col;
+        }
+        return {
+          ...col,
+          onCell: (record: Book) => ({
+            record,
+            inputType: col.dataIndex === 'date' ? 'number' : 'text',
+            dataIndex: col.dataIndex,
+            title: col.title,
+            editing: isEditing(record),
+            deleting: isDeleting(record),
+          }),
+        };
+      });
+    
+      return (
+        <Form form={form} component={false}>
+          <Table
+            components={{
+              body: {
+                cell: EditableCell,
+              },
+            }}
+            bordered
+            dataSource={data}
+            columns={mergedColumns}
+            rowClassName="editable-row"
+            pagination={{
+              onChange: cancel,
+            }}
+          />
+        </Form>
+      );
+    };
+    
 
-
-  render(){
-    const {booking} = this.props
-    return(
+    return (
       <div>
-        <h1>Books Page</h1>
-        <div>
-          {booking.map(book => (
-            <div>
-              <p>{book.title}</p>
-              <p>{book.author}</p>
-              <p>{book.date}</p>
-            </div>
-          ))}
-
-
+        <EditableTable/>    
       </div>
-      </div>
-
     );
   }
-
 }
+
+
+
+
 
 
 interface LinkStateProps {
-  booking:Books[];
+originData: Book[];
 }
 
 interface LinkDispatchProps{
-  startEditBook: (book : Books) => void;
+  startEditBook: (book : Book) => void;
+  startDeleteBook: (id:string) => void;
 }
+
+
 
 const mapStateToProps = (
   state: AppState,
-  ownProps: BookTableProps,
-
+  ownProps: BookTableProps
 ): LinkStateProps => ({
-  booking: state.books
-})
+originData: state.books
+
+});
 
 
-const mapDispatchToProps=(
+const mapDispatchToProps = (
   dispatch : ThunkDispatch<any,any,AppAction>,
-  ownprops: BookTableProps
+  ownProps: BookTableProps
 ): LinkDispatchProps => ({
-  startEditBook:  bindActionCreators(action.startEditBook, dispatch),
+  startEditBook: bindActionCreators(action.startEditBook, dispatch),
+  startDeleteBook: bindActionCreators(action.startDeleteBook, dispatch),
 })
 
 
+///////////////////////////////////////////
+/////BELOW IS THE Ant Design Table////////
+///////////////////////////////////////////
 
 
-///////////////////////////////////////////////
-/////////////BELOW THIS LINE IS ANT DESIGN/////
-///////////////////////////////////////////////
 
-const originData: Books[] = [];
-for (let i = 0; i < 100; i++) {
-  originData.push({
-    key: i.toString(),
-    title: `Earth ${i}`,
-    author: "",
-    date: '${i}',
-  });
-}
 interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
   editing: boolean;
+  deleting: boolean;
   dataIndex: string;
-  title: any;
+  title: string;
+  author:string;
   inputType: 'number' | 'text';
-  record: Books;
+  record: Book;
   index: number;
   children: React.ReactNode;
 }
 
 const EditableCell: React.FC<EditableCellProps> = ({
   editing,
+  deleting,
   dataIndex,
   title,
+  author,
   inputType,
   record,
   index,
@@ -136,122 +286,6 @@ const EditableCell: React.FC<EditableCellProps> = ({
   );
 };
 
-const EditableTable = () => {
-  const [form] = Form.useForm();
-  const [data, setData] = useState(originData);
-  const [editingKey, setEditingKey] = useState('');
-
-  const isEditing = (record: Books) => record.key === editingKey;
-
-  const edit = (record: Partial<Books> & { key: React.Key }) => {
-    form.setFieldsValue({ name: '', age: '', address: '', ...record });
-    setEditingKey(record.key);
-  };
-
-  const cancel = () => {
-    setEditingKey('');
-  };
-
-  const save = async (key: React.Key) => {
-    try {
-      const row = (await form.validateFields()) as Books;
-      const newData = [...data];
-      const index = newData.findIndex(item => key === item.key);
-      if (index > -1) {
-        const Books = newData[index];
-        newData.splice(index, 1, {
-          ...Books,
-          ...row,
-        });
-        setData(newData);
-        setEditingKey('');
-      } else {
-        newData.push(row);
-        setData(newData);
-        setEditingKey('');
-      }
-    } catch (errInfo) {
-      console.log('Validate Failed:', errInfo);
-    }
-  };
-
-  const columns = [
-    {
-      title: 'name',
-      dataIndex: 'name',
-      width: '25%',
-      editable: true,
-    },
-    {
-      title: 'age',
-      dataIndex: 'age',
-      width: '15%',
-      editable: true,
-    },
-    {
-      title: 'address',
-      dataIndex: 'address',
-      width: '40%',
-      editable: true,
-    },
-    {
-      title: 'operation',
-      dataIndex: 'operation',
-      render: (_: any, record: Books) => {
-        const editable = isEditing(record);
-        return editable ? (
-          <span>
-            <a href="javascript:;" onClick={() => save(record.key)} style={{ marginRight: 8 }}>
-              Save
-            </a>
-            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-              <a>Cancel</a>
-            </Popconfirm>
-          </span>
-        ) : (
-          <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
-            Edit
-          </Typography.Link>
-        );
-      },
-    },
-  ];
-
-  const mergedColumns = columns.map(col => {
-    if (!col.editable) {
-      return col;
-    }
-    return {
-      ...col,
-      onCell: (record: Books) => ({
-        record,
-        inputType: col.dataIndex === 'age' ? 'number' : 'text',
-        dataIndex: col.dataIndex,
-        title: col.title,
-        editing: isEditing(record),
-      }),
-    };
-  });
-
-  return (
-    <Form form={form} component={false}>
-      <Table
-        components={{
-          body: {
-            cell: EditableCell,
-          },
-        }}
-        bordered
-        dataSource={data}
-        columns={mergedColumns}
-        rowClassName="editable-row"
-        pagination={{
-          onChange: cancel,
-        }}
-      />
-    </Form>
-  );
-};
 
 
-export default connect(mapStateToProps, mapDispatchToProps) (BookTable)
+export default connect(mapStateToProps, mapDispatchToProps) (BookTable);
